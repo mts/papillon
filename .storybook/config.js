88a84f3e1efbabbbba3a2c0ef50d65c9bb8b98be
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-16 17:04:43 +0200

    Extends storybook configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { configure } from '@storybook/react'
+import { configure, addDecorator } from '@storybook/react'
 import { setOptions } from '@storybook/addon-options'
-// import '../modules/papillon/index.scss'
+import '../modules/papillon/index.scss'
 import { version } from '../modules/papillon/package.json'
 
 setOptions({
   name: `Papillon v${version}`
 })
+
+addDecorator(story => (
+  <div className='p-4'>
+    {story()}
+  </div>
+))
 
 const contexts = [
   require.context('../modules', true, /stories.*\.js$/),
