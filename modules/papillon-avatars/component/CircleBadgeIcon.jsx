7cec19aad4f26b42998f3b16ddda68f0552aa47f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-19 21:47:55 +0200

    Refactors Adds CircleBadgeIcon

--- modules/papillon-avatars/component/CircleBadgeIcon.jsx
+++ modules/papillon-avatars/component/CircleBadgeIcon.jsx
@@ -3,11 +3,20 @@ import React from 'react';
 import {string, number} from 'prop-types';
 import Octicon, {getIconByName} from '@githubprimer/octicons-react'
 
-const CircleBadgeIcon = ({className, icon}) =>
-  <Octicon icon={getIconByName(icon)} scale={1} className={className} />;
+const CircleBadgeIcon = ({className, icon}) => (
+  <Octicon
+    icon={getIconByName(icon)}
+    scale={1}
+    className={`CircleBadge-icon ${className}`}
+  />
+);
+
+CircleBadgeIcon.defaultProps = {
+  className: ''
+};
 
 CircleBadgeIcon.propTypes = {
-  className: string.isRequired,
+  className: string,
   icon: string.isRequired
 };
 
