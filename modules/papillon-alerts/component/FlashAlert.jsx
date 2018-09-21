9c8c59cf3da7f13e888a9c2dd0926b2375c29177
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-21 03:55:08 +0200

    Refactors FlashAlert and implements stories

--- modules/papillon-alerts/component/FlashAlert.jsx
+++ modules/papillon-alerts/component/FlashAlert.jsx
@@ -1,12 +1,18 @@
 import '../index.scss';
 import React from 'react';
 import {string, node} from 'prop-types';
+import Octicon, {getIconByName} from '@githubprimer/octicons-react'
 
-const FlashAlert = ({className, children}) => (
-  <div class={`flash ${className}`}>
+const FlashAlert = ({className, children, icon}) => (
+  <div className={`flash ${className}`}>
+    {icon ? <span className="mr-3"><Octicon size='small' icon={getIconByName(icon)} /></span> : null}
     {children}
   </div>
 );
+
+FlashAlert.defaultProps = {
+  icon: ''
+};
 
 FlashAlert.defaultProps = {
   className: ''
@@ -14,7 +20,8 @@ FlashAlert.defaultProps = {
 
 FlashAlert.propTypes = {
   className: string.isRequired,
-  children: node.isRequired
+  children: node.isRequired,
+  icon: string
 };
 
 export default FlashAlert;
