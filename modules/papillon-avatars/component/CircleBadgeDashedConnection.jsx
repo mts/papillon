34f6b8d5e470e3d9bd1d96fca6d1091e4d3a72d5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-27 17:05:42 +0300

    Corrects CircleBadgeDashedConnection component

--- modules/papillon-avatars/component/CircleBadgeDashedConnection.jsx
+++ modules/papillon-avatars/component/CircleBadgeDashedConnection.jsx
@@ -5,14 +5,18 @@ import {string, node} from 'prop-types';
 const CircleBadgeDashedConnection = ({children}) => (
   <div className="DashedConnection">
     <ul className="d-flex list-style-none flex-justify-between" aria-label="A sample GitHub workflow">
-      {children.map((child) => (
-        <li className="CircleBadge CircleBadge--small" aria-label="GitHub">
+      {children.map((child, index) => (
+        <li key={index} className="CircleBadge CircleBadge--small" aria-label="GitHub">
           {child}
         </li>
       ))}
     </ul>
   </div>
 );
+
+CircleBadgeDashedConnection.defaultProps = {
+  className: ''
+};
 
 CircleBadgeDashedConnection.propTypes = {
   className: string.isRequired,
