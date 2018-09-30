f9cf3210aaacda1dec6d722407016db4c69f60e7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-30 22:10:06 +0300

    Extends stories with UnderlineNav with Counter

--- modules/papillon-navigation/component/UnderlineNav/stories.js
+++ modules/papillon-navigation/component/UnderlineNav/stories.js
@@ -29,7 +29,37 @@ const items = [
   }
 ];
 
-const button = <div className="UnderlineNav-actions"><a className="btn">Button</a></div>;
+const counter = <span class="Counter">10</span>;
+
+const itemsWithIcons = [
+  { href: '#url',
+    role: 'tab',
+    title: 'Item 1',
+    text: 'Item 1',
+    icon: 'tools'
+  },
+  { href: '#url',
+    role: 'tab',
+    title: 'Item 2',
+    text: 'Item 2',
+    icon: 'tools',
+    counter
+  },
+  { href: '#url',
+    role: 'tab',
+    title: 'Item 3',
+    text: 'Item 3',
+    icon: 'tools'
+  },
+  { href: '#url',
+    role: 'tab',
+    title: 'Item 4',
+    text: 'Item 4',
+    icon: 'tools'
+  }
+];
+
+const action = <div className="UnderlineNav-actions"><a className="btn">Button</a></div>;
 
 storiesOf('Navigation', module)
   .add('UnderlineNav', () => {
@@ -45,7 +75,7 @@ storiesOf('Navigation', module)
       <UnderlineNav
         aria={aria}
         items={items}
-        action={button}
+        action={action}
         selectedIndex={0}
       />
     )
@@ -65,7 +95,16 @@ storiesOf('Navigation', module)
         aria={aria}
         className="UnderlineNav--right"
         items={items}
-        action={button}
+        action={action}
+        selectedIndex={0}
+      />
+    )
+  })
+  .add('UnderlineNav with Counter', () => {
+    return (
+      <UnderlineNav
+        aria={aria}
+        items={itemsWithIcons}
         selectedIndex={0}
       />
     )
