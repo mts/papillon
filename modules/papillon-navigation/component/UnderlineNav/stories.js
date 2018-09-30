cc35ab3ad21d2f9d1e0c096773accc8dcd2cc60e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-30 18:44:15 +0300

    Adds UnderlineNav-actions story

--- modules/papillon-navigation/component/UnderlineNav/stories.js
+++ modules/papillon-navigation/component/UnderlineNav/stories.js
@@ -2,18 +2,45 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import UnderlineNav from './UnderlineNav';
 
+const items = [
+  { href: '#url',
+    role: 'tab',
+    title: 'Item 1',
+    text: 'Item 1'
+  },
+  { href: '#url',
+    role: 'tab',
+    title: 'Item 2',
+    text: 'Item 2'
+  },
+  { href: '#url',
+    role: 'tab',
+    title: 'Item 3',
+    text: 'Item 3'
+  },
+  { href: '#url',
+    role: 'tab',
+    title: 'Item 4',
+    text: 'Item 4'
+  }
+];
+
+const html =  <div class="UnderlineNav-actions"><a class="btn">Button</a></div>;
+
 storiesOf('Navigation', module)
   .add('UnderlineNav', () => {
-    const items = [
-      { href: '#url', role: 'tab', title: 'Item 1', text: 'Item 1' },
-      { href: '#url', role: 'tab', title: 'Item 2', text: 'Item 2' },
-      { href: '#url', role: 'tab', title: 'Item 3', text: 'Item 3' },
-      { href: '#url', role: 'tab', title: 'Item 4', text: 'Item 4' }
-    ];
-
     return (
       <UnderlineNav
         items={items}
+        selectedIndex={0}
+      />
+    )
+  })
+  .add('UnderlineNav-actions', () => {
+    return (
+      <UnderlineNav
+        items={items}
+        html={html}
         selectedIndex={0}
       />
     )
