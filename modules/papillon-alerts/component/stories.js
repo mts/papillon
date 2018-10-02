45b0e5e00baadc9970b255f21216a9828ec61a1f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-02 16:00:10 +0200

    Applies withNotes hoc from @storybook/addon-notes

--- modules/papillon-alerts/component/stories.js
+++ modules/papillon-alerts/component/stories.js
@@ -15,16 +15,27 @@ import {
   fullWidthFlash
 } from './flavors';
 import { withInfo } from '@storybook/addon-info';
+import { withNotes } from '@storybook/addon-notes';
 
 storiesOf('Flash alerts', module)
-  .add('Default', withInfo('Default')(() => flashAlertDefault))
-  .add('Multiple paragraphs', withInfo('Multiple paragraphs')(() => multipleParagraphs))
-  .add('Flash messages', withInfo('Flash messages')(() => flashMessages))
-  .add('flash-warn', withInfo('flash-warn')(() => flashWarn))
-  .add('flash-error', withInfo('flash-error')(() => flashError))
-  .add('flash-success', withInfo('flash-success')(() => flashSuccess))
-  .add('flash-icon', withInfo('flash-icon')(() => flashIcon))
-  .add('with-dismiss', withInfo('with-dismiss')(() => withDismiss))
-  .add('with-action-button', withInfo('with-action-button')(() => withActionButton))
-  .add('full-width-flash', withInfo('full-width-flash')(() => fullWidthFlash))
+  .add('Default', withNotes('Must render FlashAlert ~ Default')
+      (withInfo('Default')(() => flashAlertDefault)))
+  .add('Multiple paragraphs', withNotes('Must render FlashAlert ~ Multiple paragraphs')
+      (withInfo('Multiple paragraphs')(() => multipleParagraphs)))
+  .add('Flash messages', withNotes('Must render FlashMessages ~ Flash messages')
+      (withInfo('Flash messages')(() => flashMessages)))
+  .add('flash-warn', withNotes('Must render FlashAlert ~ flash-warn')
+    (withInfo('flash-warn')(() => flashWarn)))
+  .add('flash-error', withNotes('Must render FlashAlert ~ flash-error')
+    (withInfo('flash-error')(() => flashError)))
+  .add('flash-success', withNotes('Must render FlashAlert ~ flash-success')
+    (withInfo('flash-success')(() => flashSuccess)))
+  .add('flash-icon', withNotes('Must render FlashAlert ~ flash-icon')
+    (withInfo('flash-icon')(() => flashIcon)))
+  .add('with-dismiss', withNotes('Must render FlashAlert ~ flash-dismiss')
+    (withInfo('with-dismiss')(() => withDismiss)))
+  .add('with-action-button', withNotes('Must render FlashAlert ~ with-action-button')
+    (withInfo('with-action-button')(() => withActionButton)))
+  .add('full-width-flash', withNotes('Must render FlashAlert ~ full-width-flash')
+    (withInfo('full-width-flash')(() => fullWidthFlash)))
 ;
