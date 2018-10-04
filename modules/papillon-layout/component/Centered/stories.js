aa27eb5b0b41c31678ece309dfe487cadd74364e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-04 15:06:08 +0200

    Removes component folder

--- modules/papillon-layout/component/Centered/stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Centered from './Centered';
-
-storiesOf('Layout', module)
-  .add('Centered', () => (
-    <Centered />
-  ))
-;
