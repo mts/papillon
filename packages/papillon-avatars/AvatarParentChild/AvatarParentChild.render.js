c237974a3200fbe9c194676eb53457f9b908b19d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-05 14:13:33 +0200

    Lints AvatarParentChild

--- packages/papillon-avatars/AvatarParentChild/AvatarParentChild.render.js
+++ packages/papillon-avatars/AvatarParentChild/AvatarParentChild.render.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 import Avatar from '../Avatar';
 import AvatarParentChild from './AvatarParentChild';
-import { action } from '@storybook/addon-actions';
 
 const srcAvatar = "https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png";
 const srcCircleBadge = "https://user-images.githubusercontent.com/334891/30004619-adc8ca86-90a0-11e7-8ccc-2322261d2ab1.png";
@@ -9,7 +9,8 @@ const srcCircleBadge = "https://user-images.githubusercontent.com/334891/3000461
 const avatarParentChild = (
   <AvatarParentChild className="float-left">
     <Avatar alt="parent" src={srcAvatar} width={48} height={48} />
-    <Avatar className="avatar-child" alt="child" src={srcAvatar} width={20} height={20} />
+    <Avatar className="avatar-child" alt="child" src={srcAvatar} width={20}
+height={20} />
   </AvatarParentChild>
 );
 
--- packages/papillon-avatars/AvatarParentChild/AvatarParentChild.story.js
+++ packages/papillon-avatars/AvatarParentChild/AvatarParentChild.story.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withNotes } from '@storybook/addon-notes';
 import {
   avatarParentChild,
 } from './AvatarParentChild.render';
-import { withInfo } from '@storybook/addon-info';
-import { withNotes } from '@storybook/addon-notes';
 
 storiesOf('Avatar/AvatarParentChild', module)
   .add('avatar-parent-child', withNotes('Must render Avatar ~ avatar-parent-child')
