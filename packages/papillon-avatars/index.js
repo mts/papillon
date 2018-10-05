04ad8cecdbd97241adf26f55b362166126403e24
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-05 12:51:46 +0200

    Exports default all components

--- packages/papillon-avatars/index.js
+++ packages/papillon-avatars/index.js
@@ -1,3 +1,15 @@
 import Avatar from './Avatar';
+import AvatarParentChild from './AvatarParentChild';
+import CircleBadge from './CircleBadge';
+import CircleBadgeDashedConnection from './CircleBadgeDashedConnection';
+import CircleBadgeIcon from './CircleBadgeIcon';
+import CircleBadgeImage from './CircleBadgeImage';
 
-export default Avatar;
+export default {
+  Avatar,
+  AvatarParentChild,
+  CircleBadge,
+  CircleBadgeDashedConnection,
+  CircleBadgeIcon,
+  CircleBadgeImage,
+};
