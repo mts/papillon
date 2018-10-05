57d198977373db4d2f1b78fbd0e0417d62999dfa
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-05 13:39:49 +0200

    Relocates Grid

---
+++ packages/papillon-layout/Grid/Grid.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  grid
+} from './Grid.render';
+
+describe('<Layout />', () => {
+  let wrapper;
+
+  describe('Instance', () => {
+    test('must be an instance of Grid', () => {
+      wrapper = renderMount(grid);
+      expect(wrapper.find('Grid')).toHaveLength(1);
+    });
+  });
+
+  describe('Snaphot', () => {
+    test('must match grid', () => {
+      expect(global.renderToJSON(grid)).toMatchSnapshot();
+    });
+  });
+});
