5a1018a26f5d23375df6c644da506ad987f4776e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-05 12:54:52 +0200

    Relocates Avatar

---
+++ packages/papillon-avatars/Avatar/Avatar.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  avatar,
+  avatarSmall,
+  avatarStackMore,
+  avatarStackThreePlus,
+  avatarStackTwo,
+  avatarStackRight,
+} from './Avatar.render';
+
+describe('<Avatar />', () => {
+  let wrapper;
+
+  describe('Instance', () => {
+    test('must be an instance of Avatar', () => {
+      wrapper = renderMount(avatar);
+      expect(wrapper.find('Avatar')).toHaveLength(1);
+    });
+  });
+
+  describe('Snaphot', () => {
+    test('must match avatar', () => {
+      expect(global.renderToJSON(avatar)).toMatchSnapshot();
+    });
+
+    test('must match avatarSmall', () => {
+      expect(global.renderToJSON(avatarSmall)).toMatchSnapshot();
+    });
+
+    test('must match avatarStackMore', () => {
+      expect(global.renderToJSON(avatarStackMore)).toMatchSnapshot();
+    });
+
+    test('must match avatarStackThreePlus', () => {
+      expect(global.renderToJSON(avatarStackThreePlus)).toMatchSnapshot();
+    });
+
+    test('must match avatarStackTwo', () => {
+      expect(global.renderToJSON(avatarStackTwo)).toMatchSnapshot();
+    });
+
+    test('must match avatarStackRight', () => {
+      expect(global.renderToJSON(avatarStackRight)).toMatchSnapshot();
+    });
+  });
+});
