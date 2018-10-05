7c300d7086c0a7963bfa79ac237675bb30c2a118
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-05 13:40:03 +0200

    Relocates Container

---
+++ packages/papillon-layout/Container/Container.render.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from './Container';
+import { action } from '@storybook/addon-actions';
+
+const container = (
+  <Container className="border">Container</Container>
+);
+
+export {
+  container,
+};
