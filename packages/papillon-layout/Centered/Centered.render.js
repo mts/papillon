fa11c8160b93549b599c7107f1c03836081c2892
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-05 13:40:10 +0200

    Relocates Centered

---
+++ packages/papillon-layout/Centered/Centered.render.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Centered from './Centered';
+import { action } from '@storybook/addon-actions';
+
+const centered = (
+  <Centered />
+);
+
+export {
+  centered,
+};
