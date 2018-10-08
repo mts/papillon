0eccec470a4f3706c7a87c2b7140e9add178073d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-08 16:05:00 +0200

    Reuses storybook base configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,27 +4,32 @@ import { setOptions } from '@storybook/addon-options';
 import '../packages/papillon/index.scss';
 import { version } from '../packages/papillon/package.json';
 
-setOptions({
-  name: `Papillon v${version}`,
-  url: 'https://github.com/mts/papillon',
-  showAddonPanel: true,
-  addonPanelInRight: false
-});
+const setup = () => {
+  setOptions({
+    name: `Papillon v${version}`,
+    url: 'https://github.com/mts/papillon',
+    showAddonPanel: true,
+    addonPanelInRight: false
+  });
 
-addDecorator(story => (
-  <div className='p-4'>
-    {story()}
-  </div>
-));
+  addDecorator(story => (
+    <div className='p-4'>
+      {story()}
+    </div>
+  ));
 
-const contexts = [
-  require.context('../packages', true, /story.*\.js$/),
-];
+  const contexts = [require.context('../packages', true, /story.*\.js$/)];
 
-configure(() => {
-  contexts.forEach(context => {
-    context.keys()
-      .filter(key => !key.includes('node_modules'))
-      .forEach(context)
-  })
-}, module);
+  const loadStories = () => {
+    contexts.forEach(context => {
+      context.keys()
+        .filter(key => !key.includes('node_modules'))
+        .forEach(context)
+    })
+  };
+
+  configure(loadStories, module);
+};
+
+export default setup;
+
