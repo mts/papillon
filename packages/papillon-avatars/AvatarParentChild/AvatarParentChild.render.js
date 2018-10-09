40aee9c4643e45d30e61221d616310db6c8859d3
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-09 14:39:24 +0200

    Requires develop and release version AvatarParentChild

--- packages/papillon-avatars/AvatarParentChild/AvatarParentChild.render.js
+++ packages/papillon-avatars/AvatarParentChild/AvatarParentChild.render.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Avatar from '../Avatar';
+
+const Avatar = process.env.NODE_ENV === 'develop'
+  ? require('../Avatar').default
+  : require('../lib/papillon-avatars').Avatar;
 
 const AvatarParentChild = process.env.NODE_ENV === 'develop'
   ? require('./AvatarParentChild').default
-  : require('../lib/papillon-avatars').AvatarParentChildvatar;
+  : require('../lib/papillon-avatars').AvatarParentChild;
 
 const srcAvatar = "https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png";
 
