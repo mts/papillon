209a58e43c6329f672b6e744efd6b99898765083
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-05 18:23:03 +0100

    Remove comments

--- packages/papillon-alerts/lib/FlashMessages/FlashMessages.render.js
+++ packages/papillon-alerts/lib/FlashMessages/FlashMessages.render.js
@@ -8,10 +8,6 @@ const FlashMessages = process.env.NODE_ENV === 'develop'
   ? require('./FlashMessages').default
   : require('../../build/papillon-alerts').FlashMessages
 
-console.log('~process.env.NODE_ENV~', process.env.NODE_ENV);
-console.log('~FlashAlert~', FlashAlert);
-console.log('~FlashMessages~', FlashMessages);
-
 const flashMessages = (
   <div className='p-4'>
     <FlashMessages>
