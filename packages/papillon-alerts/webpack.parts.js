ede4841955f1e3d18568ddf9441819fef90fe1b2
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-15 20:42:31 +0100

    extend babel-loader options

--- packages/papillon-alerts/webpack.parts.js
+++ packages/papillon-alerts/webpack.parts.js
@@ -58,7 +58,21 @@ exports.loadJavaScript = ({ include, exclude } = {}) => ({
         include,
         exclude,
 
-        use: "babel-loader",
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-syntax-dynamic-import',
+              '@babel/plugin-transform-runtime',
+              '@babel/plugin-transform-spread',
+            ],
+          },
+        },
       },
     ],
   },
