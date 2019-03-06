039bbde56e6476cc3d945e316d40f3f437e41ad5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-03-06 23:01:05 +0100

    add and use @storybook/theming

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,44 +1,49 @@
 import React from 'react'
-import { configure, addDecorator, addParameters } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { withNotes } from '@storybook/addon-notes'
-// import '../packages/papillon/index.scss';
-import { version } from '../lerna.json';
+import { create } from '@storybook/theming'
+// import '../packages/papillon/index.scss'
+import { version } from '../lerna.json'
 
 const setup = () => {
-  addDecorator(withInfo);
-  addDecorator(withNotes);
+  addDecorator(withInfo)
+  addDecorator(withNotes)
   addParameters({
     options: {
-      brandTitle: `Papillon v${version}`,
-      brandUrl: 'https://github.com/mts/papillon',
+      theme: create({
+        base: 'light',
+        brandTitle: `Papillon v${version}`,
+        brandUrl: 'https://github.com/mts/papillon'
+      }),
+      isFullscreen: false,
       showNav: true,
       showPanel: false,
       sidebarAnimations: false
-    }
+    },
   })
 
   addDecorator(story => (
     <div className='p-4'>
       {story()}
     </div>
-  ));
+  ))
 
-  const contexts = [require.context('../packages', true, /story*\.js$/)];
+  const contexts = [require.context('../packages', true, /story*\.js$/)]
 
   const loadStories = () => {
-    require('./welcomeStory');
+    require('./welcomeStory')
     contexts.forEach(context => {
       context.keys()
         .filter(key => !key.includes('node_modules'))
         .forEach(context)
     })
-  };
+  }
 
-  configure(loadStories, module);
-};
+  configure(loadStories, module)
+}
 
 setup()
 
-export default setup;
+export default setup
 
