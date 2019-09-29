224064e837b8da0b079dc31fc86163bedb0ed57d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-09-29 10:03:29 +0200

    configure jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,35 @@
 module.exports = {
-  testRegex: '.test\\.js$',
+  testMatch: ['**/?(*.)test.js?(x)'],
   roots: ['<rootDir>'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
   collectCoverageFrom: [
-    '!**/*.md',
-    'packages/**/*.js',
-    'packages/**/*.jsx',
+    '!**/.storybook/**',
+    '!**/.storybook-static/**',
+    '!**/coverage/**',
     '!**/node_modules/**',
+    'packages/**/*.{js,jsx}',
     '!packages/**/index.js',
     '!packages/**/*.test.js',
     '!packages/**/*.story.js',
     '!packages/**/*.render.js',
+    '!packages/**/build/**',
+    '!packages/**/explorer/**',
+    '!packages/**/node_modules/**',
+    '!packages/**/webpack*',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '\\.(css|less|scss)$': 'identity-obj-proxy',
   },
   moduleFileExtensions: ['js', 'jsx'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 54,
+      functions: 88,
+      lines: 80,
+    },
+  },
 }
