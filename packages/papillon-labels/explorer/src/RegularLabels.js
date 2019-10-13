8f6491bdecdd2723817b502b0f79e56d9c59c9d5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-13 18:05:33 +0200

    import variants

--- packages/papillon-labels/explorer/src/RegularLabels.js
+++ packages/papillon-labels/explorer/src/RegularLabels.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Label } from 'papillon-labels'
+import { Label, variants } from 'papillon-labels'
 // import { Label, variants } from '../../build/papillon-labels'
 
 export const RegularLabels = () => {
