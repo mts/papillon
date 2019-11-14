ff6e3597af037d5a27c3238206f4df1402350830
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-11-14 15:08:04 +0100

    FlashAlert use classnames

--- packages/papillon-alerts/lib/FlashAlert/FlashAlert.jsx
+++ packages/papillon-alerts/lib/FlashAlert/FlashAlert.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import cx from 'classnames'
 import Octicon, { getIconByName } from '@githubprimer/octicons-react'
 import { propTypes, defaultProps } from './FlashAlert.props'
 import '@primer/css/alerts/index.scss'
 import '@primer/css/core/index.scss'
 
 export const FlashAlert = ({ className, children, icon }) => (
-  <div className={`flash ${className}`.trim()}>
+  <div className={cx(className, 'flash')}>
     {icon ? (
       <span className="mr-3">
         <Octicon size="small" icon={getIconByName(icon)} />
