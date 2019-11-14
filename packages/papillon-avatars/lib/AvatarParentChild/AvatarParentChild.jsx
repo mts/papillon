c3055c410763a6d55ba1637f668158024dc38557
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-11-14 15:09:59 +0100

    AvatarParentChild use classnames

--- packages/papillon-avatars/lib/AvatarParentChild/AvatarParentChild.jsx
+++ packages/papillon-avatars/lib/AvatarParentChild/AvatarParentChild.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import cx from 'classnames'
 import { propTypes, defaultProps } from './AvatarParentChild.props'
 import '@primer/css/avatars/index.scss'
 import '@primer/css/core/index.scss'
 
-export const AvatarParentChild = ({ className, children }) => <div className={`avatar-parent-child ${className}`.trim()}>{children}</div>
+export const AvatarParentChild = ({ className, children }) => <div className={cx(className, 'avatar-parent-child')}>{children}</div>
 
 AvatarParentChild.propTypes = propTypes
 
